073ef676cd4975c552a24c00d89cef70b77dd1e1
yuerxin1997 <yuerxin1997@163.com>
2023-10-26 21:32:38 +0800

    rm

--- train_reward.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python reward.py  \
-    --num_train_epochs 10 \
-    --train_file /home/xxx/data/ppo_weibo/weiboReward/train_reward.json \
-    --dev_file /home/xxx/data/ppo_weibo/weiboReward/dev_reward.json \
-    --output_dir /home/xxx/output/gpt2_rewardModel
